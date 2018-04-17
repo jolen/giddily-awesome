7867dc6f1b97dcf4826b40879edcd22d42456ed6
Ann Abayon <ajuabayon@gmail.com>
2018-04-17 10:39:54 +0800

    Make the header transparent

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,1 +1,26 @@
+$(window).on("scroll", function() {
+    if ($(window).scrollTop() > $(".page__splash-screen").height() - $(".page__nav-bar").height()) {
+        $(".page__nav-bar").removeClass("page__nav-bar--transparent");
+    } else {
+       $(".page__nav-bar").addClass("page__nav-bar--transparent");
+    }
+})
+
+
+// $(window).resize(function() {
+//     var winHeight = $(window).height();
+//     var winWidth = $(window).width();
+
+//     if (winWidth > winHeight) {
+//         $(".page__splash-screen").removeClass("portrait");
+//         $(".page__splash-screen").addClass("landscape");
+//     } else {
+//         $(".page__splash-screen").removeClass("landscape");
+//         $(".page__splash-screen").addClass("portrait");
+//     }
+
+//     console.log("h: " + winHeight + ", w: " + winWidth);
+
+// });
+
 $(document).foundation();
