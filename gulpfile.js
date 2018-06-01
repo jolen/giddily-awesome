3aa103282e08cb23ded7e2bd5947c852db5b9d45
Emil Purugganan <emilgp@gmail.com>
2018-06-01 21:23:07 +0800

    added panini workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var $    = require('gulp-load-plugins')();
+var panini = require('panini');
 
 var sassPaths = [
   'node_modules/foundation-sites/scss',
@@ -19,6 +20,19 @@ gulp.task('sass', function() {
     .pipe(gulp.dest('css'));
 });
 
-gulp.task('default', ['sass'], function() {
+gulp.task('html', function() {
+  gulp.src('src/pages/**/*.html')
+    .pipe(panini({
+      root: 'src/pages/',
+      layouts: 'src/layouts/',
+      partials: 'src/partials/',
+      helpers: 'src/helpers/',
+      data: 'src/data/'
+    }))
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('default', ['sass', 'html'], function() {
   gulp.watch(['scss/**/*.scss'], ['sass']);
+  gulp.watch(['./src/{layouts,partials,helpers,data}/**/*.html'], [panini.refresh]);
 });
